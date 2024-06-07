564f2a602421f1e144a42ef3429370034f8c18c0
knuts983 <knuts983@umn.edu>
2024-06-07 00:51:39 -0500

    Python3 to run collectstatic

--- start-container.sh
+++ start-container.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#  -v /etc/letsencrypt/live/cknutson.org-0001/cert.pem:/app/cert.pem:ro \
-#  -v /etc/letsencrypt/live/cknutson.org-0001/privkey.pem:/app/privkey.pem:ro \
 
 docker run -d --name=bookend --privileged \
   -v "${PWD}"/library_project/db.sqlite3:/app/db.sqlite3:rw \
+  --env DJANGO_SECRET_KEY="RANDOM_SECRET_KEY_VALUE" \
+  --env ALLOWED_HOST="YOUR_HOSTNAME" \
   --rm -p 8080:8080 \
   rogueraptor7/bookend:latest
