a86a62df0026a0bedd1407bd044538747168258c
knuts983 <knuts983@umn.edu>
2024-06-07 00:44:48 -0500

    Updated allowedhosts

--- library_project/run-server.sh
+++ library_project/run-server.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
+source ../.env
+
 python3 manage.py migrate
 python3 manage.py makemigrations
-#python3 manage.py runserver 0.0.0.0:8080
 python manage.py collectstatic --no-input
-gunicorn library_project.wsgi:application --bind 0.0.0.0:8080
-#python3 manage.py runserver_plus 0.0.0.0:443 --cert-file cert.pem --key-file privkey.pem
+gunicorn library_project.wsgi:application --bind "$ALLOWED_HOST":8080
