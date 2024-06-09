e9604cc6abd5a0c520ac042256afefa2c959033b
knuts983 <knuts983@umn.edu>
2024-06-09 01:24:01 -0500

    Changed background style to greens and checkin/out to a different red

--- library_project/run-server.sh
+++ library_project/run-server.sh
@@ -4,3 +4,4 @@ python3 manage.py migrate
 python3 manage.py makemigrations
 python3 manage.py collectstatic --no-input
 gunicorn library_project.wsgi:application --bind 0.0.0.0:8080
+#python3 manage.py runserver 0.0.0.0:8080
